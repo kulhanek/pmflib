e7b464603c7110b6dff68b21fd88f92b0cf2615e
Petr Kulhanek <kulhanek@chemi.muni.cz>
2016-04-02 16:56:32 +0200

    small update

--- CMakeClean.sh
+++ CMakeClean.sh
@@ -9,15 +9,17 @@ case $MODE in
     "quick")
         rm -rf `find . -name CMakeFiles`
         rm -rf `find . -name CMakeCache.txt`
-        rm -rf `find . -name cmake_install.cmake`
+        rm -rf `find . -name '*.cmake'`
         rm -rf `find . -name Makefile`
         rm -rf `find . -name '*~'`
         rm -rf `find . -name '*.o'`
+        rm -rf `find . -name '*.mod'`
+        rm -rf `find . -name '*genmod.f90'`
     ;;
     "deep")
         rm -rf `find . -name CMakeFiles`
         rm -rf `find . -name CMakeCache.txt`
-        rm -rf `find . -name cmake_install.cmake`
+        rm -rf `find . -name '*.cmake'`
         rm -rf `find . -name Makefile`
         rm -rf `find . -name '*~'`
         rm -rf `find . -name '*.o'`
@@ -25,6 +27,7 @@ case $MODE in
         rm -rf `find . -name '*.so'`
         rm -rf `find . -name '*.so.*'`
         rm -rf `find . -name '*.mod'`
+        rm -rf `find . -name '*genmod.f90'`
         rm -rf bin/*
     ;;
 esac
