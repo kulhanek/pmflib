1fecf9a78c0b1e98f6f9c3037cb60211a0ba3665
Petr Kulhanek <kulhanek@fes.ncbr.muni.cz>
2017-06-06 22:49:45 +0200

    versioning update

---
+++ src/lib/cpmf/PMFMainHeaderConfig.hpp
@@ -0,0 +1,2 @@
+#define PMF_VERSION         "5.x"
+#define PMF_BUILDTIME       "DATE"
--- src/lib/fpmf/common/pmf_ver.f90
+++ src/lib/fpmf/common/pmf_ver.f90
@@ -1,36 +1,8 @@
-!===============================================================================
-! PMFLib - Library Supporting Potential of Mean Force Calculations
-!-------------------------------------------------------------------------------
-!    Copyright (C) 2011-2015 Petr Kulhanek, kulhanek@chemi.muni.cz
-!    Copyright (C) 2013-2015 Letif Mones, lam81@cam.ac.uk
-!
-!    This library is free software; you can redistribute it and/or
-!    modify it under the terms of the GNU Lesser General Public
-!    License as published by the Free Software Foundation; either
-!    version 2.1 of the License, or (at your option) any later version.
-!
-!    This library is distributed in the hope that it will be useful,
-!    but WITHOUT ANY WARRANTY; without even the implied warranty of
-!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-!    Lesser General Public License for more details.
-!
-!    You should have received a copy of the GNU Lesser General Public
-!    License along with this library; if not, write to the Free Software
-!    Foundation, Inc., 51 Franklin Street, Fifth Floor, 
-!    Boston, MA  02110-1301  USA
-!===============================================================================
-
 module pmf_ver
-
 use pmf_sizes
 use pmf_constants
-
 implicit none
 !-------------------------------------------------------------------------------
-
-character(80),parameter   :: PMFLIBVER   = 'PMFLib 5.0.SVNVERSION (DATE)'
-
+character(80),parameter   :: PMFLIBVER   = 'PMFLib 5.x (DATE)'
 !-------------------------------------------------------------------------------
-
 end module pmf_ver
-
