c83bb4b94b12610f4fd21edc964b89eed4d19e4c
Petr Kulhanek <kulhanek@fes.ncbr.muni.cz>
2017-06-06 22:52:56 +0200

    versioning update

--- src/lib/cpmf/PMFMainHeaderConfig.hpp
+++ src/lib/cpmf/PMFMainHeaderConfig.hpp
@@ -1,2 +1,2 @@
-#define PMF_VERSION         "5.x"
-#define PMF_BUILDTIME       "DATE"
+#define PMF_VERSION         "5.85.1fecf9a"
+#define PMF_BUILDTIME       "2017-06-06 22:52"
--- src/lib/fpmf/common/pmf_ver.f90
+++ src/lib/fpmf/common/pmf_ver.f90
@@ -3,6 +3,6 @@ use pmf_sizes
 use pmf_constants
 implicit none
 !-------------------------------------------------------------------------------
-character(80),parameter   :: PMFLIBVER   = 'PMFLib 5.x (DATE)'
+character(80),parameter   :: PMFLIBVER   = 'PMFLib 5.85.1fecf9a (2017-06-06 22:52)'
 !-------------------------------------------------------------------------------
 end module pmf_ver
