586c2f6dcaa8ba3ecdbd42bf653dcc7b61d4a798
Petr Kulhanek <kulhanek@chemi.muni.cz>
2018-12-13 17:40:28 +0100

    update

--- src/lib/cpmf/PMFMainHeaderConfig.hpp
+++
@@ -1,2 +0,0 @@
-#define PMF_VERSION         "5.x"
-#define PMF_BUILDTIME       "DATE"
--- src/lib/fpmf/common/pmf_ver.f90
+++
@@ -1,8 +0,0 @@
-module pmf_ver
-use pmf_sizes
-use pmf_constants
-implicit none
-!-------------------------------------------------------------------------------
-character(80),parameter   :: PMFLIBVER   = 'PMFLib 5.x (DATE)'
-!-------------------------------------------------------------------------------
-end module pmf_ver
