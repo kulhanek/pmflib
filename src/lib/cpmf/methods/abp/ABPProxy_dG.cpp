8152f9dd492f3dce9eca7cd85c43f72b0f8bc619
Petr Kulhanek <kulhanek@chemi.muni.cz>
2021-06-25 16:12:01 +0200

    refactoring

---
+++ src/lib/cpmf/methods/abp/ABPProxy_dG.cpp
@@ -0,0 +1,100 @@
+// =============================================================================
+// PMFLib - Library Supporting Potential of Mean Force Calculations
+// -----------------------------------------------------------------------------
+//    Copyright (C) 2021 Petr Kulhanek, kulhanek@chemi.muni.cz
+//    Copyright (C) 2008 Petr Kulhanek, kulhanek@enzim.hu
+//                       Martin Petrek, petrek@chemi.muni.cz
+//
+//     This program is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation; either version 2 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along
+//     with this program; if not, write to the Free Software Foundation, Inc.,
+//     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// =============================================================================
+
+#include <ABPProxy_dG.hpp>
+
+//------------------------------------------------------------------------------
+
+using namespace std;
+
+//==============================================================================
+//------------------------------------------------------------------------------
+//==============================================================================
+
+CABPProxy_dG::CABPProxy_dG(void)
+{
+    Require = "ABP";
+    Provide = "ABP dG(x)";
+}
+
+//------------------------------------------------------------------------------
+
+CABPProxy_dG::~CABPProxy_dG(void)
+{
+
+}
+
+//==============================================================================
+//------------------------------------------------------------------------------
+//==============================================================================
+
+int CABPProxy_dG::GetNumOfSamples(int ibin) const
+{
+    if( Accu == NULL ){
+        RUNTIME_ERROR("Accu is NULL");
+    }
+
+    return( Accu->GetData("NSAMPLES",ibin) );
+}
+
+//------------------------------------------------------------------------------
+
+double CABPProxy_dG::GetValue(int ibin,EProxyRealm realm) const
+{
+    if( Accu == NULL ){
+        RUNTIME_ERROR("Accu is NULL");
+    }
+
+    double mtdpot = Accu->GetData("MTDPOT",ibin);
+
+    switch(realm){
+        // -------------------
+        case(E_PROXY_VALUE):
+            return( - mtdpot );
+        // -------------------
+        default:
+            RUNTIME_ERROR("unsupported realm");
+    }
+
+    return( mtdpot );
+}
+
+//    const double Rgas = 0.0019872065;
+//
+//    double m = 1.0;
+//    for(int i=0; i < Accu->GetNumOfBins(); i++){
+//        double pop = Accu->GetPop(i);
+//        if( pop > m ) m = pop;
+//    }
+//    for(int i=0; i < Accu->GetNumOfBins(); i++){
+//        double pop = Accu->GetPop(i);
+//        double ene = - Accu->GetTemperature()*Rgas*log(pop/m);
+//        FES->SetEnergy(i,ene);
+//        FES->SetNumOfSamples(i,Accu->GetNumberOfABPSamples(i));
+//    }
+
+//==============================================================================
+//------------------------------------------------------------------------------
+//==============================================================================
+
+
+
---
+++ src/lib/cpmf/methods/abp/ABPProxy_dG.hpp
@@ -0,0 +1,53 @@
+#ifndef ABPProxy_dGH
+#define ABPProxy_dGH
+// =============================================================================
+// PMFLib - Library Supporting Potential of Mean Force Calculations
+// -----------------------------------------------------------------------------
+//    Copyright (C) 2021 Petr Kulhanek, kulhanek@chemi.muni.cz
+//    Copyright (C) 2008 Petr Kulhanek, kulhanek@enzim.hu
+//                       Martin Petrek, petrek@chemi.muni.cz
+//
+//     This program is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation; either version 2 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along
+//     with this program; if not, write to the Free Software Foundation, Inc.,
+//     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// =============================================================================
+
+#include <PMFMainHeader.hpp>
+#include <EnergyProxy.hpp>
+
+//------------------------------------------------------------------------------
+
+/** \brief return free energy from MTD accumulator
+*/
+
+class PMF_PACKAGE CABPProxy_dG : public CEnergyProxy {
+public:
+// constructor and destructor -------------------------------------------------
+    CABPProxy_dG(void);
+    ~CABPProxy_dG(void);
+
+//------------------------------------------------------------------------------
+    // get number of samples
+    virtual int GetNumOfSamples(int ibin) const;
+
+    // get energy derivative and its error
+    virtual double GetValue( int ibin,EProxyRealm realm) const;
+};
+
+//------------------------------------------------------------------------------
+
+typedef std::shared_ptr<CABPProxy_dG>    CABPProxy_dG_Ptr;
+
+//------------------------------------------------------------------------------
+
+#endif
