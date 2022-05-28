65664e1b44528f4f7de0ec2dbfb2f3e39f9587ae
Petr Kulhanek <kulhanek@chemi.muni.cz>
2022-05-28 14:10:57 +0200

    test

--- src/lib/fpmf/methods/abf/abf_core.f90
+++ src/lib/fpmf/methods/abf/abf_core.f90
@@ -368,12 +368,12 @@ subroutine abf_core_force_5pV2()
                 ! velocity part
                 v1 = v1 + (     - zdhist(m,j,i,hist_len-0) + 8.0d0*zdhist(m,j,i,hist_len-1) &
                            -8.0d0*zdhist(m,j,i,hist_len-3)       + zdhist(m,j,i,hist_len-4) ) * &
-                           (       - vhist(m,j,hist_len-0) + 7.0d0*vhist(m,j,hist_len-1) &
-                             + 7.0d0*vhist(m,j,hist_len-2)       - vhist(m,j,hist_len-3))
+                           (       - vhist(m,j,hist_len-0) + 9.0d0*vhist(m,j,hist_len-1) &
+                             + 9.0d0*vhist(m,j,hist_len-2)       - vhist(m,j,hist_len-3))
             end do
         end do
         pxif(i) = (1.0d0/24.0d0)*f1*ifdtx
-        pxiv(i) = (1.0d0/144.0d0)*v1*ifdtx
+        pxiv(i) = (1.0d0/192.0d0)*v1*ifdtx
     end do
 
     ! subroutine abf_core_register_rawdata(cvs,ficf,sicf,vicf,licf,bicf,epot,erst,ekin)
