61d6f13c4268da3e1911bf557263571a4890979d
Petr Kulhanek <kulhanek@chemi.muni.cz>
2025-08-15 21:47:40 +0200

    CST test

--- src/lib/fpmf/methods/cst/cst_accu.f90
+++ src/lib/fpmf/methods/cst/cst_accu.f90
@@ -948,7 +948,7 @@ subroutine cst_accu_add_dhTds
 
     do i=1,NumOfAllCONs
 
-        llam  = lambdahist(i,hist_len+hist_fidx)
+        llam  = lambdahist(i,hist_len+hist_fidx+1) ! FIXME
 
         if( fentropy ) then
             call cst_accu_add_data_OMI(llam, invn, mlamtds(i), m2lamtds(i), dlam1, dlam2)
