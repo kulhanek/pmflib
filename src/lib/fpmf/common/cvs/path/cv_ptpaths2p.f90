cbecb0938b7e6086a251779b774217ce97b2b9f3
Petr Kulhanek <kulhanek@chemi.muni.cz>
2026-05-28 23:18:52 +0200

    update, refactoring

--- src/lib/fpmf/common/cvs/path/cv_ptpaths2p.f90
+++ src/lib/fpmf/common/cvs/path/cv_ptpaths2p.f90
@@ -19,6 +19,8 @@
 !    Boston, MA  02110-1301  USA
 !===============================================================================
 
+! PTPATHS2P - path-progress coordinate along a line generated from two points
+
 module cv_ptpaths2p
 
 use pmf_sizes
--- src/lib/fpmf/common/cvs/path/cv_ptpaths2pn.f90
+++ src/lib/fpmf/common/cvs/path/cv_ptpaths2pn.f90
@@ -19,6 +19,8 @@
 !    Boston, MA  02110-1301  USA
 !===============================================================================
 
+! PTPATHS2PN - path-progress coordinate  along bidirectional line from origin + direction
+
 module cv_ptpaths2pn
 
 use pmf_sizes
