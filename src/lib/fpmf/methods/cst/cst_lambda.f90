440af9a4b014925d940e59ae98b13ab32d462321
Petr Kulhanek <kulhanek@chemi.muni.cz>
2026-06-12 18:15:36 +0200

    CST Copyright

--- src/lib/fpmf/methods/cst/cst_lambda.f90
+++ src/lib/fpmf/methods/cst/cst_lambda.f90
@@ -1,7 +1,7 @@
 !===============================================================================
 ! PMFLib - Library Supporting Potential of Mean Force Calculations
 !-------------------------------------------------------------------------------
-!    Copyright (C) 2025 Petr Kulhanek, kulhanek@chemi.muni.cz
+!    Copyright (C) 2025-2026 Petr Kulhanek, kulhanek@chemi.muni.cz
 !
 !    This library is free software; you can redistribute it and/or
 !    modify it under the terms of the GNU Lesser General Public
