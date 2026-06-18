a7cf6884bd775b5f9e704fbb5fc7dbe9abbf76d7
Petr Kulhanek <kulhanek@chemi.muni.cz>
2026-06-18 21:14:51 +0200

    bugfix

--- src/lib/fpmf/methods/cst/cst_control.F90
+++ src/lib/fpmf/methods/cst/cst_control.F90
@@ -125,7 +125,7 @@ subroutine cst_control_read_con(prm_fin)
     call pmf_ctrl_read_logical(prm_fin,'fint_der',fint_der)
 
     call pmf_ctrl_read_integer(prm_fin,'ftds_icfsol',ftds_icfsol,'I12')
-    call pmf_ctrl_check_integer_in_range('CST','ftds_icfsol',ftds_icfsol,1,4)
+    call pmf_ctrl_check_integer_in_range('CST','ftds_icfsol',ftds_icfsol,1,6)
 
     call pmf_ctrl_read_integer(prm_fin,'fpmf_sdiv_S',fpmf_sdiv_S,'I12')
     call pmf_ctrl_read_logical(prm_fin,'fpmf_sdiv_qr',fpmf_sdiv_qr)
